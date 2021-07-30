0ca76cd9bb7807d563fca8ca72cfefde46fafeac
Brett Dorrans <hello@lapidist.net>
2021-07-30 20:04:13 +0100

    feat: allow specifying placeholder

--- README.md
+++ README.md
@@ -6,11 +6,15 @@ A GitHub Action that fetches the latest git tag within a repo and writes this to
 
 ### `filename`
 
-**Required** - The filename to write the version tag to. Default `/.VERSION`
+**Required** - The filename to write the version tag to.
+
+### `placeholder`
+
+**Optional** - The filename to write the version tag to. Defaults to `${VERSION}`
 
 ## Example usage
 
-Commit a file containing `${VERSION}` to the root of your repository. This string will be replaced with the latest git tag in the CI pipeline, e.g. in the example below you should commit this to a file named `.VERSION`
+Commit a file named `.VERSION` containing `${VERSION}` to the root of your repository. This string will be replaced with the latest git tag in the CI pipeline.
 
 ```
 name: Write Version to File
@@ -26,8 +30,8 @@ jobs:
     steps:
     - uses: actions/checkout@master
     - name: Update version
-      id: version
       uses: brettdorrans/write-version-to-file@master
       with:
         filename: '/.VERSION'
+        placeholder: '${VERSION}'
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,41 @@
 #!/bin/sh
-set -eou pipefail
+set -eo pipefail
+
+error() {
+    echo -e "\x1b[1;31m$1\e[0m $2"
+}
+
+log() {
+    echo -e "\x1b[1;32m$1\e[0m $2"
+}
 
 filename=$1
+
+log "File Name:" "$filename"
+
+if [ -z "$2" ]; then
+    placeholder="\${VERSION}"
+else
+    placeholder=$2
+fi
+
+log "Placeholder:" "$placeholder"
 
 if test -f "$filename"; then
     content=$(cat "$filename")
 else
-    echo "Version file not found"
+    error "Version file not found! Looked for:" "$filename"
     exit 1;
 fi
 
+log "File Content:" "\n$content"
+
 git fetch --tags --force
-latestVersionTag=$(git describe --tags "$(git rev-list --tags --max-count=1)");
-echo "File Content: $content"
-echo "Updating Version To: $latestVersionTag"
-updatedContent=$(cat "$filename" | sed "s/\${VERSION}/$latestVersionTag/g")
-echo "Updated Content: $updatedContent"
+latestVersionTag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+
+log "Replacing placeholder with:" "$latestVersionTag"
+
+updatedContent=$(< "$filename" sed "s/$placeholder/$latestVersionTag/g")
 echo "$updatedContent" > "$filename"
+
+log "Updated File Content:" "\n$updatedContent"
